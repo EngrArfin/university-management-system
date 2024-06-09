7d2fb3a31ca989cbed5caa8aee1a664d4ea9ac3f
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2024-06-10 00:04:15 +0600

    joi setup

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,4 +1,5 @@
 import validator from 'validator';
+
 import { Schema, model } from 'mongoose';
 import {
   Gurdian,
--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,10 +1,66 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
-import { error } from 'console';
+import Joi from 'joi';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
+    //creating a schimma validation using joi
+
+    const userNameSchema = Joi.object({
+      firstName: Joi.string()
+        .trim()
+        .max(20)
+        .required()
+        .regex(/^[A-Z][a-zA-Z]*$/),
+      middleName: Joi.string().trim().optional(),
+      lastName: Joi.string()
+        .trim()
+        .required()
+        .regex(/^[a-zA-Z0-9]*$/),
+    });
+
+    // Guardian Joi Schema
+    const guardianSchema = Joi.object({
+      fatherName: Joi.string().required(),
+      fatherOccupation: Joi.string().required(),
+      fatherContactNo: Joi.string().required(),
+      motherName: Joi.string().required(),
+      motherOccupation: Joi.string().required(),
+      motherContactNo: Joi.string().required(),
+    });
+
+    // Local Guardian Joi Schema
+    const localGuardianSchema = Joi.object({
+      name: Joi.string().required(),
+      occupation: Joi.string().required(),
+      contactNo: Joi.string().required(),
+      address: Joi.string().required(),
+    });
+
+    // Student Joi Schema
+    const studentSchema = Joi.object({
+      id: Joi.string().required(),
+      name: userNameSchema.required(),
+      gender: Joi.string().valid('male', 'female').required(),
+      email: Joi.string().email().required(),
+      dateOfBirth: Joi.string().optional(),
+      bloodGroup: Joi.string()
+        .valid('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')
+        .optional(),
+      presentAddress: Joi.string().required(),
+      permanentAddress: Joi.string().required(),
+      contactNo: Joi.string().required(),
+      emergencyContactNo: Joi.string().required(),
+      guardian: guardianSchema.required(),
+      localGuardian: localGuardianSchema.required(),
+      profileImg: Joi.string().optional(),
+      isActive: Joi.string().valid('active', 'blocked').default('active'),
+    });
+
     const { student: studentData } = req.body;
+    const { error, value } = studentSchema.validate(studentData);
+    console.log(error, value);
+
     const result = await StudentServices.createStudentIntoDB(studentData);
     res.status(200).json({
       success: true,
